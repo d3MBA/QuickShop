c5f7660d133ca921b7e4abcacf36c4b2ade32bbd
d3MBA <demba.mc24@gmail.com>
2025-02-17 15:11:49 +0000

    added login page

---
+++ login_page.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Log in - QuickShop</title>
+</head>
+<body>
+
+<h2>Log in</h2>
+
+<p><strong>Email address:</strong></p>
+<input type="text" id="emailAddress" placeholder="Enter your email address...">
+<p><strong>Password:</strong></p>
+<input type="text" id="password" placeholder="Enter your password...">
+
+<br><br>
+<button>Log in</button>
+
+
+<br><br>
+<a href="signup_page.php">No account yet?</a>
+
+
+</body>
+</html>
