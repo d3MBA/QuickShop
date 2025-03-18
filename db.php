34ae5531c20cdecb276985cea5113573bd6aeb27
d3MBA <demba.mc24@gmail.com>
2025-03-18 22:45:08 +0000

    some db connection fixes

--- db.php
+++ db.php
@@ -5,11 +5,11 @@
     $dbname = "quickshop";
     $port = 3308;
 
-    // create connection
-    $conn = new mysqli($host, $user, $password, $dbname, $port = 3308);
+// create connection
+$conn = new mysqli($host, $user, $password, $dbname, $port = 3308);
 
-    // check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+// check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 ?>
